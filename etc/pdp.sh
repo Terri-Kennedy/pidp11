b2849406993c69bb71669bd3022c0951cf4cba1f
Terri Kennedy <87054869+Terri-Kennedy@users.noreply.github.com>
2025-09-08 03:52:26 -0400

    Improve error reporting

--- etc/pdp.sh
+++ etc/pdp.sh
@@ -1,5 +1,13 @@
 procs=`screen -ls pidp11 | egrep '[0-9]+\.pidp11' | wc -l`
-echo $procs
+if [ $procs -eq 0 ]; then
+	echo No PDP-11 emulator detected as running - check with pidp11control.
+	exit 1
+fi
+
+if [ $procs -gt 1 ]; then
+	echo More than one screen session found; results may be unpredictable.
+fi
+
 if [ $procs -ne 0 ]; then
 	screen -d -r
 fi
